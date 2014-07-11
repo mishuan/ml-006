51379d0b4b09594a7a1728d00ad9b66fcd256ee3
DrCalculus <yuanshihao1994@gmail.com>
2014-07-11 01:02:38 -0400

    Assignment 2 finished

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 %
 
 J = (1/m)*sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta)));
-grad = (1/m)*sum((sigmoid(X*theta)-y).*X);
+grad = (1/m)*sum((sigmoid(X*theta)-y).*X)
 
 % =============================================================
 
--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -10,6 +10,7 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
+n = size(grad);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,10 +18,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (1/m)*sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta))) + (0.5*lambda/m)*sum(theta(2:length(theta)).^2);
 
-
-
-
+grad = ((1/m)*(sigmoid(X*theta)-y)'*X)'+(lambda/m)*theta;
+grad(1) = (1/m)*sum((sigmoid(X*theta)-y).*X(:,1));
 
 % =============================================================
 
