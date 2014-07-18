c29acaf188dbee7880b60ff2d9d9712d8bfa008e
DrCalculus <yuanshihao1994@gmail.com>
2014-07-17 23:50:32 -0400

    Finished Assignment 4 -neural networks with given theta

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -9,7 +9,7 @@ num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
-
+X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -21,13 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+z2 = X*Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m, 1) a2];
+z3 = a2*Theta2';
+h = sigmoid(z3);
+[temp, p] = max(h, [], 2);
 
 % =========================================================================
 
